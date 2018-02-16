2bc21f445c7e0f2f1ec75a9320d3e331b5763e12
Yifan Zhu <llag9810@outlook.com>
2018-02-17 01:13:38 +0800

    Update zhong.py

--- zhong.py
+++ zhong.py
@@ -172,7 +172,7 @@ def fuq(bot,update):
     if text in welcome_msg or text in reply_msg:
         update.message.reply_text(random.choice(reply_msg))
 
-    if re.match("[亲,抱]*.*[rgy,饶耕宇].*[亲,抱,啪啪啪]*", text):
+    if re.match("[亲,抱]+.*[rgy,饶耕宇].*", text) or re.match(".*[rgy,饶耕宇].*[亲,抱,啪啪啪]+.*", text):
         if random.choice(range(2)) == 1:
             re_list = ["那么喜欢的话……钟钟就把rgy给你啦~",
                   "亲亲rgy",
