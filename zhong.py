694082c2072d3c721ca65dd6d8055db44fa07982
KiriKira <32393884+KiriKira@users.noreply.github.com>
2018-02-17 11:31:27 +0800

    update rgy

--- zhong.py
+++ zhong.py
@@ -172,13 +172,15 @@ def fuq(bot,update):
     if text in welcome_msg or text in reply_msg:
         update.message.reply_text(random.choice(reply_msg))
 
-    if text == "亲亲rgy":
-        if random.choice(range(2)) == 1:
-            re = ["亲亲",
-                  "亲亲rgy",
-                  "No gay, plz!",
-                  "More gay, plz!"]
-            update.message.reply_text(random.choice(re))
+    m = re.match(r"(.*(?P<act>(亲|抱|捏)+).*(?P<person>rgy|饶耕宇|学姐|三爷|kiri|千千|大风车|洛天依)).*", text)
+    if not m:
+        m = re.match(r".*((?P<person>rgy|饶耕宇|学姐|三爷|kiri|千千|大风车|洛天依).*(?P<act>(亲|抱|啪啪啪|捏)+)).*", text)
+    if m:
+        re_list = [m.group("act"),
+                   m.group("act") + m.group("person"),
+                   "No gay, plz!",
+                   "More gay, plz!"]
+        update.message.reply_text(random.choice(re_list))
 
 def main():
     """Run bot."""
